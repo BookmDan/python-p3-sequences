e233e08f1e60fa44b1074c74e7b93ae6836d850c
BookmDan <danoh329@gmail.com>
2023-11-11 17:50:35 -0800

    pass

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    # fib_seq = [0,1]
+    # while len(fib_seq) < length:
+    #     next = fib_seq[-1] + fib_seq[-2]
+    #     fib_seq.append(next)
+
+    # print(fib_seq)
+    fibonacci_sequence = []
+    a, b = 0, 1
+
+    for _ in range(length):
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+
+    print(f'{fibonacci_sequence}')
